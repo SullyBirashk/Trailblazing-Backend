9fd0c16e741caaae0e5c730e233f81a827099010
Sully Birashk <sullybirashk@Sullys-MBP.home>
2024-03-02 17:45:33 -0800

    Feat: Can now create a User

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::UsersController < ApplicationController
 
+    def index
+        users = User.all
+
+        render json: users
+    end
+
     def create
         user = User.new(user_params)
+
         if user.save
           render json: user, status: :created
         else
@@ -12,7 +19,7 @@ class Api::V1::UsersController < ApplicationController
       private
     
       def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        params.permit(:name, :email, :password, :password_confirmation)
       end
 
 end
