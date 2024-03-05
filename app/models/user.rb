50fe1b329cc27f8f14806552c08b184186917b6d
Sully Birashk <sullybirashk@Sullys-MacBook-Pro.local>
2024-03-04 19:28:17 -0800

    Remove Bcrypt to replace with Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_secure_password
 
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
